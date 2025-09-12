59bf080eb0898fa9ae27a1f80335f42d0fc28795
RajAleti <97768579+RajAleti@users.noreply.github.com>
2025-09-12 17:54:46 -0500

    add the files for code deploy

---
+++ simple-python-app/scripts/start_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull rajaleti/simple-python-flask-app
+
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 rajaleti/simple-python-flask-app
